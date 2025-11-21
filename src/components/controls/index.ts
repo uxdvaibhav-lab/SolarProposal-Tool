a58e8b2682aa0a02494fe27af2d1bcfdf6d47e65
Vaibhav Agarwal <uxd.vaibhav@gmail.com>
2025-11-21 16:56:14 +0530

    Updates

--- src/components/controls/index.ts
+++
@@ -1,4 +0,0 @@
-export { DocumentControlMenu } from './DocumentControlMenu';
-export { DesignControlMenu } from './DesignControlMenu';
-export { CollaborationMenu } from './CollaborationMenu';
-export { ProfileMenu } from './ProfileMenu';
